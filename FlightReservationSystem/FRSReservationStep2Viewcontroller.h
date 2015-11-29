c4407ddc9dd896457d63a828740b2e30ad74a15a
laksmikanth somavarapu <laksmikanthsomavarapu@laksmikanths-macbook-pro.local>
2015-11-28 16:43:54 -0800

    Added FRSReservationStep2ViewController

---
+++ FlightReservationSystem/FRSReservationStep2Viewcontroller.h
@@ -0,0 +1,13 @@
+//
+//  FRSReservationStep2Viewcontroller.h
+//  FlightReservationSystem
+//
+//  Created by laksmikanth somavarapu on 28/11/2015.
+//  Copyright © 2015 Revanth Kumar. All rights reserved.
+//
+
+#import "FRSBaseViewController.h"
+
+@interface FRSReservationStep2Viewcontroller : FRSBaseViewController
+
+@end
---
+++ FlightReservationSystem/FRSReservationStep2Viewcontroller.m
@@ -0,0 +1,37 @@
+//
+//  FRSReservationStep2Viewcontroller.m
+//  FlightReservationSystem
+//
+//  Created by laksmikanth somavarapu on 28/11/2015.
+//  Copyright © 2015 Revanth Kumar. All rights reserved.
+//
+
+#import "FRSReservationStep2Viewcontroller.h"
+
+@interface FRSReservationStep2Viewcontroller ()
+
+@end
+
+@implementation FRSReservationStep2Viewcontroller
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
